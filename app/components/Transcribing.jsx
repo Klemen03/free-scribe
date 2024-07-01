7b4f441f116eaf76901ae2d3b41285a4d7f67ba8
Klemen03 <klemenkrklec10@gmail.com>
2024-07-01 11:56:31 +0200

    build the loading animation

--- app/components/Transcribing.jsx
+++ app/components/Transcribing.jsx
@@ -1,6 +1,6 @@
 const Transcribing = ({ downloading }) => {
   return (
-    <div className="flex items-center flex-col text-center justify-center gap-10 md:gap-14 py-24 p-4">
+    <div className="flex items-center flex-1 flex-col text-center justify-center gap-10 md:gap-14 pb-24 p-4">
       <div className="flex flex-col gap-2 sm:gap-4">
         <h1 className="font-semibold text-4xl sm:text-5xl md:text-6xl">
           <span className="text-blue-500 bold">Transcribing</span>
